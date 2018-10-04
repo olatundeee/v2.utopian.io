d907130be2bb660f825fcd1ba5d3bf07761e9051
Grégory <gregory.latinier@gmail.com>
2018-10-05 00:29:21 +0200

    Finish the html

--- packages/client/src/pages/projects/create-edit/create-edit.vue
+++ packages/client/src/pages/projects/create-edit/create-edit.vue
@@ -3,13 +3,13 @@ import { Notify } from 'quasar'
 import { mapActions } from 'vuex'
 import { minLength, required, requiredUnless, url } from 'vuelidate/lib/validators'
 import { LicencesMixin } from 'src/mixins/licences'
-import UFileUploader from 'src/components/project/file-uploader/file-uploader'
+import UWysiwyg from 'src/components/form/wysiwyg'
 
 export default {
   name: 'u-page-projects-create-edit',
   mixins: [LicencesMixin],
   components: {
-    UFileUploader
+    UWysiwyg
   },
   data () {
     return {
@@ -22,8 +22,9 @@ export default {
         website: '',
         docs: '',
         license: '',
-        images: [],
+        medias: [],
         description: '',
+        details: 'Write everything about your project…',
         tags: []
       }
     }
@@ -37,13 +38,11 @@ export default {
       website: {url},
       docs: {url},
       license: {required},
-      images: {
-        /*
-        required,
-        minLength: minLength(1)
-        */
+      medias: {
+        hasImage: function () { return this.$refs.uploader.files.length > 0 }
       },
       description: {required},
+      details: {required},
       tags: {
         required,
         minLength: minLength(3)
@@ -63,15 +62,12 @@ export default {
     searchGithubRepositoryWrapper (query, done) {
       this.searchGithubRepository(query).then(done)
     },
-    handleImageUpload (uploadUrl) {
-      this.updateFormPercentage('images')
-      this.project.images = [uploadUrl]
-    },
     updateFormPercentage (field) {
       this.$v.project[field].$touch()
       const fields = Object.keys(this.$v.project.$params)
       const completed = fields.reduce((count, key) => {
         if (this.$v.project[key].$error) return count
+        if (key === 'medias' && this.$refs.uploader && this.$refs.uploader.files.length > 0) return count + 1
         if (key === 'repository' && this.project.closedSource) return count + 1
         if (typeof this.project[key] === 'string' && this.project[key] !== '') return count + 1
         if (Array.isArray(this.project[key]) && this.project[key].length > 0) return count + 1
